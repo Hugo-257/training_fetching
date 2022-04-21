86be7089f9f2ef15943af2c53fcf464abd9f5938
Hugo-257 <ntore2000@gmail.com>
2022-04-22 01:36:14 +0200

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables
 
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,8 +16,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: 'Poppins'),
-      home: const SplashScreen(),
+      theme: ThemeData(
+        fontFamily: 'Poppins',
+      ),
+      home: AnimatedSplashScreen(
+        duration: 3000,
+        splash: Text(
+          "Groot",
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 52.0,
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.bold),
+        ),
+        nextScreen: HomeScreen(),
+        splashTransition: SplashTransition.fadeTransition,
+        pageTransitionType: PageTransitionType.bottomToTop,
+        backgroundColor: Color(0xFFea6f79),
+      ),
     );
   }
 }
@@ -25,59 +43,73 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xFFea6f79),
-        toolbarHeight: 0, // status bar color
-      ),
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: 200,
-                  width: 150,
-                  child: Image.asset('asset/images/smiley.png'),
-                )
-              ],
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Groot",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 52.0,
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 0.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Your daily dose of humor",
-                  style: TextStyle(
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 200,
+                width: 150,
+                child: Image.asset('asset/images/smiley.png'),
+              )
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Groot",
+                style: TextStyle(
                     color: Colors.white,
+                    fontSize: 52.0,
                     fontFamily: 'Poppins',
-                    fontSize: 24.0,
-                  ),
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 0.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Your daily dose of humor",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Poppins',
+                  fontSize: 24.0,
                 ),
-              ],
-            )
-          ],
+              ),
+            ],
+          )
+        ],
+      ),
+      decoration: BoxDecoration(color: Color(0xFFea6f79)),
+      height: MediaQuery.of(context).size.height,
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: Text("Hello"),
         ),
-        decoration: BoxDecoration(color: Color(0xFFea6f79)),
-        height: MediaQuery.of(context).size.height,
       ),
     );
   }
